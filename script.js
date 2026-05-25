874a3cbb0bb3ae2cb03a992476be6ae11518ed8b
Yuli <glezglezyuli@gmail.com>
2026-05-25 23:08:01 +0200

    mini chat ia

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
-function start() {
-    alert("🔥 Funciona perfectamente");
+function sendMessage() {
+
+    const input = document.getElementById("userInput");
+    const chatBox = document.getElementById("chatBox");
+
+    const messageText = input.value;
+
+    if (messageText === "") {
+        return;
+    }
+
+    // Mensaje usuario
+    const userMessage = document.createElement("div");
+
+    userMessage.classList.add("message");
+    userMessage.classList.add("user");
+
+    userMessage.textContent = messageText;
+
+    chatBox.appendChild(userMessage);
+
+    // Respuesta IA
+    const botMessage = document.createElement("div");
+
+    botMessage.classList.add("message");
+    botMessage.classList.add("bot");
+
+    botMessage.textContent = "🤖 Estoy aprendiendo a responder.";
+
+    setTimeout(() => {
+        chatBox.appendChild(botMessage);
+
+        chatBox.scrollTop = chatBox.scrollHeight;
+    }, 500);
+
+    input.value = "";
 }
